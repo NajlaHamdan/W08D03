bd6b560d281d9f4b647274f24aa1dc0d5fa1ae1d
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-11-30 23:06:31 +0300

    modify deleteTodosSingleUser

--- db/models/role.js
+++ db/models/role.js
@@ -5,7 +5,8 @@ const role = new mongoose.Schema({
     type: String,
     required: true,
   },
-  permissions: {type:Array,required:true}
+  permissions: {type:Array,required:true},
+  role: [{ type: mongoose.Schema.Types.ObjectId ,ref:"role"}]
 });
 
 module.exports = mongoose.model("role", role);
