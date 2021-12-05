d753615a89b5d71e91ece3b1986b51f9a1fc814b
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-12-05 20:36:45 +0300

    fixed

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -3,7 +3,7 @@ const userModel = require("./../../db/models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const salt = Number(process.env.SALT);
-const secret = process.env.SECRETKEY;
+const secret = process.env.SECRET;
 
 const register = async (req, res) => {
   const { email, password, role } = req.body;
@@ -19,17 +19,19 @@ const register = async (req, res) => {
       res.status(201).json(result);
     })
     .catch((err) => {
+      console.log(err);
       res.status(404).json(err);
     });
 };
 
 const login = (req, res) => {
   const { email, password } = req.body;
-  userModel
+   userModel
     .findOne({ email }) //with find will return email and say not valid if it is valid
     .then(async (result) => {
       if (result) {
         if (result.email == email) {
+          // console.log("hhhi");
           const payload = {
             role: result.role,
           };
@@ -37,20 +39,23 @@ const login = (req, res) => {
             expiresIn: "60m",
           };
           const token = await jwt.sign(payload, secret, options);
-          //   console.log(token);
+          console.log(token);
           const decrybtedPass = await bcrypt.compare(password, result.password);
+          // console.log(decrybtedPass);
+
           if (decrybtedPass) {
-            res.status("200").json(result);
+            // console.log("hhhi");
+            res.status(200).json(result);
           } else {
-            //   console.log("hi");
+            console.log("hi");
             res.status("404").json("email or password is not valid");
           }
         } else {
-          // console.log("hi");
-          res.status("404").json("email or password is not valid");
+          console.log("hi");
+          res.status(404).json("email or password is not valid");
         }
       } else {
-        res.status("404").json("does not exist");
+        res.status(404).json("does not exist");
       }
     })
     .catch((err) => res.status("404").json(err));
